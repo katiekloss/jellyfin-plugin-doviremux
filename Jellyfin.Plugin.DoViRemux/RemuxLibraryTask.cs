2cb0f6b7877c8bd874de284cb1eaaf73807a7cac
Katie Kloss <kloss.katherine@gmail.com>
2025-04-30 10:57:29 -0400

    RemuxLibraryTask: skip truehd audio.

--- Jellyfin.Plugin.DoViRemux/RemuxLibraryTask.cs
+++ Jellyfin.Plugin.DoViRemux/RemuxLibraryTask.cs
@@ -6,12 +6,15 @@ using MediaBrowser.Controller.MediaEncoding;
 using MediaBrowser.Controller.Persistence;
 using MediaBrowser.Controller.Streaming;
 using MediaBrowser.Model.Tasks;
+using Microsoft.Extensions.Logging;
+
 namespace Jellyfin.Plugin.DoViRemuxPlugin;
 
 public class RemuxLibraryTask(IItemRepository _itemRepo,
                               IMediaSourceManager _sourceManager,
                               ITranscodeManager _transcodeManager,
-                              IPluginManager _pluginManager)
+                              IPluginManager _pluginManager,
+                              ILogger<RemuxLibraryTask> _logger)
     : IScheduledTask
 {
     public string Name => "Remux Dolby Vision MKVs";
@@ -24,7 +27,7 @@ public class RemuxLibraryTask(IItemRepository _itemRepo,
 
     public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
     {
-        return [new TaskTriggerInfo(){ Type = TaskTriggerInfo.TriggerDaily, TimeOfDayTicks = 0 }];
+        return [new TaskTriggerInfo() { Type = TaskTriggerInfo.TriggerDaily, TimeOfDayTicks = 0 }];
     }
 
     public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
@@ -43,7 +46,14 @@ public class RemuxLibraryTask(IItemRepository _itemRepo,
 
         foreach (var item in allItems.Items)
         {
-            await ProcessOneItem(item, cancellationToken);
+            try
+            {
+                await ProcessOneItem(item, cancellationToken);
+            }
+            catch (Exception x)
+            {
+                _logger.LogWarning(x, "Failed to process {ItemId}", item.Id);
+            }
         }
     }
 
@@ -54,8 +64,8 @@ public class RemuxLibraryTask(IItemRepository _itemRepo,
         var streams = _sourceManager.GetMediaStreams(item.Id);
         var doviStream = streams.FirstOrDefault(s => s.Type == MediaBrowser.Model.Entities.MediaStreamType.Video
                                              && s.DvProfile.HasValue);
-        
-        if (doviStream is not {DvProfile: 8, DvVersionMajor: 1}) return;
+
+        if (doviStream is not { DvProfile: 8, DvVersionMajor: 1 }) return;
 
         // if there's an existing MP4 source, assume we made it.
         // also I can't decide if I like that the model object comes back with services inside it
@@ -67,6 +77,14 @@ public class RemuxLibraryTask(IItemRepository _itemRepo,
 
         var inputPath = ourSource.Path;
         var outputPath = $"{inputPath}.mp4";
+
+        // truehd isn't supported by many consumer MP4 decoders even though ffmpeg can do it
+        // since it's also dolby, it's used by a lot of DoVi media
+        var audioStream = streams.FirstOrDefault(s => s.Type == MediaBrowser.Model.Entities.MediaStreamType.Audio
+                                              && s.Codec != "truehd"
+                                              && s.Language == "eng")
+            // TODO: transcode it instead
+            ?? throw new Exception("Couldn't find an appropriate audio stream to copy");
 
         var remuxRequest = new StreamState(_sourceManager, TranscodingJobType.Progressive, _transcodeManager);
 
@@ -85,11 +103,11 @@ public class RemuxLibraryTask(IItemRepository _itemRepo,
 
         string cli = "-analyzeduration 200M -probesize 1G -fflags +genpts ";
         cli += $"-i \"{inputPath}\" ";
-        cli += "-map_metadata -1 -map_chapters -1 -threads 0 -map 0:0 -map 0:1 -map -0:s ";
+        cli += $"-map_metadata -1 -map_chapters -1 -threads 0 -map 0:0 -map 0:{audioStream.Index} -map -0:s ";
         cli += "-codec:v:0 copy -tag:v:0 dvh1 -strict -2 -bsf:v hevc_mp4toannexb -start_at_zero ";
         cli += "-codec:a:0 copy -copyts -avoid_negative_ts disabled -max_muxing_queue_size 2048 ";
         cli += $"\"{outputPath}\"";
- 
+
         var remuxCancelToken = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
         var job = await _transcodeManager.StartFfMpeg(remuxRequest, outputPath, cli, Guid.Empty, TranscodingJobType.Progressive, remuxCancelToken);
     }

f3aa75a83821e790916ba398d26f63cb40da9271
Katie Kloss <kloss.katherine@gmail.com>
2025-05-11 14:22:00 -0400

    add the final mp4box remux to the pipeline

--- Jellyfin.Plugin.DoViRemux/DownmuxWorkflow.cs
+++ Jellyfin.Plugin.DoViRemux/DownmuxWorkflow.cs
@@ -16,7 +16,7 @@ public class DownmuxWorkflow(PluginConfiguration _configuration,
         var downmuxCancellationSource = CancellationTokenSource.CreateLinkedTokenSource(token);
 
         // extract the HEVC stream...
-        var ffmpeg = Process.Start(new ProcessStartInfo("ffmpeg")
+        using var ffmpeg = Process.Start(new ProcessStartInfo("ffmpeg")
         {
             Arguments = string.Join(" ", [
                 "-y",
@@ -32,12 +32,13 @@ public class DownmuxWorkflow(PluginConfiguration _configuration,
         
         if (ffmpeg is null || ffmpeg.HasExited) throw new InvalidOperationException("FFmpeg failed to start");
 
-        var doviTool = Process.Start(new ProcessStartInfo(_configuration.PathToDoviTool)
+        // and feed it into dovi_tool, discarding the enhancement layer
+        using var doviTool = Process.Start(new ProcessStartInfo(_configuration.PathToDoviTool)
         {
             Arguments = string.Join(" ", [
-                "-m 2",
-                "convert",
-                "--discard",
+                "-m 2", // convert RPU to 8.1
+                "convert", // modify RPU
+                "--discard", // discard EL
                 "-",
                 "-o /tmp/dovi_tool.hevc"
             ]),
@@ -46,11 +47,10 @@ public class DownmuxWorkflow(PluginConfiguration _configuration,
         
         if (doviTool is null || doviTool.HasExited) throw new InvalidOperationException("dovi_tool failed to start");
 
-        doviTool.StandardInput.AutoFlush = true;
-
         var buffer = new byte[4 * 1024 * 1024];
         var bytesRead = 0;
 
+        // extremely complicated way of writing a shell pipe
         do
         {
             bytesRead = await ffmpeg.StandardOutput.BaseStream.ReadAsync(buffer, downmuxCancellationSource.Token);
@@ -59,8 +59,30 @@ public class DownmuxWorkflow(PluginConfiguration _configuration,
         }
         while (!ffmpeg.HasExited && bytesRead > 0);
 
-        await doviTool.StandardInput.FlushAsync();
         doviTool.StandardInput.Close();
+
+        // then remux the HEVC stream into an MP4 with the right DoVi side data
+        // indicating it's now profile 8.1
+        using var mp4box = Process.Start(new ProcessStartInfo(_configuration.PathToMP4Box)
+        {
+            // no clue what any of this does, except for the dvp line
+            Arguments = string.Join(" ", [
+                "-add",
+                "/tmp/dovi_tool.hevc:dvp=8.1:xps_inband:hdr=none",
+                "-brand mp42isom",
+                "-ab dby1",
+                "-no-iod",
+                "/tmp/final.mp4",
+                "-tmp /tmp"
+            ])
+        });
+
+        if (mp4box is null || mp4box.HasExited) throw new InvalidOperationException("mp4box failed to start");
+
+        while (!mp4box.HasExited)
+        {
+            await Task.Delay(1000, downmuxCancellationSource.Token);
+        }
 
         return "/tmp/final.mp4";
     }
